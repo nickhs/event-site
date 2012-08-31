d66477d00d10320b44775c26594c69d86e01a6ff
Jack Pearkes <jackpearkes+git@gmail.com>
2012-08-31 15:47:43 -0700

    Bring the vm provisioning file into the future.

--- provision.sh
+++ provision.sh
@@ -7,26 +7,36 @@ apt-get install -y libpq-dev python-dev
 easy_install pip
 # libevent
 apt-get install -y libevent-dev
-## ruby support
-apt-get install -y ruby-full
 ## get git for pip via git
-apt-get install git-core
+apt-get install -y git-core
 ## Curl
-apt-get install curl
-## Ruby & RVM
-curl -L https://get.rvm.io | bash -s stable --ruby
-## Make RVM work
-source /home/vagrant/.rvm/scripts/rvm
-## Foreman
-gem install foreman --no-ri --no-rdoc
+apt-get install -y curl
 ## PEP8
 apt-get install -y pep8
 ## pyflakes
 apt-get install -y pyflakes
 ## cmake
 apt-get install -y cmake
-## make ~/site default
-echo "cd ~/site/" >> ~/.bashrc
+## RabbitMQ
+apt-get install -y rabbitmq-server
+## Htop
+apt-get install -y htop
+# Be a vagrant user
+su vagrant
+## Ruby & RVM
+curl -L https://get.rvm.io | bash -s stable
+## Make RVM work
+source /etc/profile.d/rvm.sh
+# Install latest ruby
+/usr/local/rvm/bin/rvm install 1.9.2
+# Go forth and ruby 1.9.2
+rvm use 1.9.2
+## Foreman
+gem install foreman --no-ri --no-rdoc
+## make ~/site default and use rvm 1.9.2
+echo "cd ~/site/
+
+rvm use 1.9.2" >> ~/.bashrc
 echo "
 
 Provisioning Complete. CTRL+C if this shows for more than a few seconds...
