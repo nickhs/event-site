2e88efc4d32875ce072927d1f091edb00695b339
Nick HS <nickhs@umich.edu>
2012-08-30 22:21:52 -0700

    Lotsa changes

---
+++ eventsite/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from models import db
+from views import data_api
+import config
+
+
+app = Flask('eventsite')
+app.debug = config.DEBUG
+app.secret_key = config.SECRET_KEY
+app.host = config.HOST
+
+app.register_blueprint(data_api)
+db.init_app(app)
---
+++ eventsite/config.py
@@ -0,0 +1,4 @@
+DEBUG = True
+SECRET_KEY = "lalalallallla"
+HOST = '0.0.0.0'
+SQLALCHEMY_DATABASE_URI = "sqlite:///test/test.db"
