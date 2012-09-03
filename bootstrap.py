e16d5e93e6ca3b13a072dd332f8fe6b46819f328
Jack Pearkes <jackpearkes+git@gmail.com>
2012-09-03 14:25:22 -0700

    Update the bootstrap script to work with the city models.

--- bootstrap.py
+++ bootstrap.py
@@ -4,39 +4,25 @@ import datetime
 import random
 import faker
 
-addresses = ['565 Angel Bernal, 15324 Rancho Serena Dr, Paramount, CA 90723-4579',
-             '4106 Tucson Dr, Greensboro, NC 27406-6333',
-             '500 E Sixth St, Apt 2, Prairie City, IA 50228-8608',
-             '2600 Pine Lake Dr, Greensboro, NC 27407-6646',
-             '5302 Texoma Pkwy, Sherman, TX 75090-2112',
-             '886 Bay Ridge Ave, Brooklyn, NY 11220-5710',
-             '1318 N Vasco Rd, Livermore, CA 94551-9212',
-             '1700 W New Haven Ave, Melbourne, FL 32904-3919',
-             '16900 N Bay Rd, Sunny Isles Beach, FL 33160-4252',
-             '410 Gardenia Ln, Buffalo Grove, IL 60089-1661',
-             '1511 E 86th St, Chicago, IL 60619-6518',
-             '28 Milford St, Medway, MA 02053-1631',
-             '9402 Bentridge Ave, Potomac, MD 20854-2870',
-             '1018 Adams Ave, Apt 3C, Salisbury, MD 21804-6687',
-             '1901 Kylemore Dr, Greensboro, NC 27406-6440',
-             '201 Railroad Ave, East Rutherford, NJ 07073-1943',
-             '220 Railroad Ave, East Rutherford, NJ 07073-1943',
-             '3333 228th St SE, Bothell, WA 98021-8950',
-             '4510 W Capitol Dr, Milwaukee, WI 53216-1564',
-             '1803 S Eighth St, Rogers, AR 72756-5912',
-             '1812 Chartwell Dr, Fort Wayne, IN 46816-1382']
+addresses = [{'address': '565 Angel Bernal, 15324 Rancho Serena Dr', 'city': 'Paramount', 'state': 'CA', 'zip': '90723-4579'},
+             {'address': '4106 Tucson Dr', 'city': 'Greensboro', 'state': 'NC', 'zip': '27406-6333'},
+             {'address': '500 E Sixth St, Apt 2', 'city': 'Prairie City', 'state': 'IA', 'zip': '50228-8608'},
+             {'address': '2600 Pine Lake Dr', 'city': 'Greensboro', 'state': 'NC', 'zip': '27407-6646'},
+             {'address': '5302 Texoma Pkwy', 'city': 'Sherman', 'state': 'TX', 'zip': '75090-2112'},
+             {'address': '1318 N Vasco Rd', 'city': 'Livermore', 'state': 'CA', 'zip': '94551-9212'},
+             {'address': '1700 W New Haven Ave', 'city': 'Melbourne', 'state': 'FL', 'zip': '32904-3919'},
+             {'address': '16900 N Bay Rd', 'city': 'Sunny Isles Beach', 'state': 'FL', 'zip': '33160-4252'},
+             {'address': '410 Gardenia Ln', 'city': 'Buffalo Grove', 'state': 'IL', 'zip': '60089-1661'},
+             {'address': '1511 E 86th St', 'city': 'Chicago', 'state': 'IL', 'zip': '60619-6518'}]
 
 
 def get_start_time():
     return datetime.datetime.today() - datetime.timedelta(days=10)
 
 
-def create_full_address():
-    street = faker.address.street_address(include_secondary=True)
-    city = faker.address.city()
-    state = faker.address.us_state_abbr()
-    print '%s, %s, %s' % (street, city, state)
-    return '%s, %s, %s' % (street, city, state)
+def create_full_address(i):
+    addr = addresses[i]
+    return '%s, %s, %s, %s' % (addr['address'], addr['city'], addr['state'], addr['zip'])
 
 
 def create_owner():
@@ -46,8 +32,8 @@ def create_owner():
     return owner
 
 
-def create_city():
-    city = City(faker.address.city())
+def create_city(i):
+    city = City(addresses[i]['city'])
     print city
     db.session.add(city)
     db.session.commit()
@@ -58,11 +44,11 @@ def create_fake_event(i):
     """
     Creates a fake event for use in testing.
     """
-    event = Event(address=addresses[i],
+    event = Event(address=create_full_address(i),
                   title=' '.join(faker.lorem.words()).title(),
                   owner=create_owner(),
                   desc=faker.lorem.paragraph(),
-                  city=create_city(),
+                  city=create_city(i),
                   start_date=str(faker.date.datetime(get_start_time())),
                   end_date=str(faker.date.datetime(get_start_time())
                                + datetime.timedelta(days=5)),
@@ -76,5 +62,5 @@ if __name__ == '__main__':
     with app.test_request_context():
         db.drop_all()
         db.create_all()
-        for i in xrange(20):
+        for i in xrange(len(addresses)):
             create_fake_event(i)
