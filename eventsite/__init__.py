b8290cd74097f7529b0e369ddf76865eff580adc
Jack Pearkes <jackpearkes+git@gmail.com>
2012-09-05 17:55:41 -0700

    Add the Admin site but it doesn't work.

--- eventsite/__init__.py
+++ eventsite/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from models import db
-from views import data_api
-
+from views import data_api, admin
 
 app = Flask('eventsite')
 app.config.from_pyfile('config.py')
 
 app.register_blueprint(data_api)
+app.register_blueprint(admin)
 
 db.init_app(app)
