3b2c1aa1c57fb7e5912577d3687b0e7a30a0717d
Nick HS <nickhs@umich.edu>
2012-09-06 17:02:13 -0700

    Allow peoples to create from admin side

--- eventsite/admin.py
+++ eventsite/admin.py
@@ -4,4 +4,31 @@ from models import Event, db
 
 admin = Admin(name='Event Site')
 
-admin.add_view(ModelView(Event, db.session))
+
+class CustomView(ModelView):
+    list_columns = ('title', 'desc', 'address', 'start_date', 'end_date',
+                    'city', 'featured', 'paid', 'owner', 'link')
+
+    form_columns = list_columns
+
+    rename_columns = dict(desc='Description')
+
+    def create_model(self, form):
+        model = Event(address=form.address.data,
+                      title=form.title.data,
+                      owner=form.owner.data,
+                      city=form.city.data,
+                      start_date=str(form.start_date.data),
+                      end_date=str(form.end_date.data),
+                      desc=form.desc.data,
+                      link=form.link.data,
+                      paid=form.paid.data,
+                      featured=form.featured.data)
+        self.session.add(model)
+        self.session.commit()
+
+    def __init__(self, session):
+        super(CustomView, self).__init__(Event, session)
+
+view = CustomView(db.session)
+admin.add_view(view)
